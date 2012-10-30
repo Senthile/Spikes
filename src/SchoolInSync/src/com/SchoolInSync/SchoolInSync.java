c8f207b4788761fb0e98ba590cb1430d2fc1122c
Senthil Elayappan <senthil.elayappan@gmail.com>
2012-10-29 23:39:55 -0400

    addd the project

---
+++ src/SchoolInSync/src/com/SchoolInSync/SchoolInSync.java
@@ -0,0 +1,16 @@
+package com.SchoolInSync;
+
+import android.os.Bundle;
+import org.apache.cordova.DroidGap;
+
+public class SchoolInSync extends DroidGap {
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //setContentView(R.layout.main);
+        super.loadUrl("file:///android_asset/www/index.html");
+    }
+}
