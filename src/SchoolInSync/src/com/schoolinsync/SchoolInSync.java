1cb2ce1d005e554b66da92d54216d11fed7ae5cd
Senthil Elayappan <senthil.elayappan@gmail.com>
2012-10-30 20:23:24 -0400

    base project issues fixed

--- src/SchoolInSync/src/com/schoolinsync/SchoolInSync.java
+++ src/SchoolInSync/src/com/schoolinsync/SchoolInSync.java
@@ -1,15 +1,17 @@
 package com.schoolinsync;
 
-import android.app.Activity;
 import android.os.Bundle;
+import org.apache.cordova.DroidGap;
 
-public class SchoolInSync extends Activity {
+public class SchoolInSync extends DroidGap {
     /**
      * Called when the activity is first created.
      */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        //setContentView(R.layout.main);
+        super.loadUrl("file:///android_asset/www/index.html");
+
     }
 }
